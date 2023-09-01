987d2cee2a4622919f0f2c6b362897fcd8ef809a
Alberto Llamedo <alberto.llamedo@atos.net>
2023-09-01 11:03:18 +0200

    Improve TestQuery

--- aggregator/aggregator_test.go
+++ aggregator/aggregator_test.go
@@ -6,9 +6,15 @@ import (
 )
 
 func TestQuery(t *testing.T) {
-	os.Setenv("PROMETHEUS_ADDRESS", "http://localhost:10902")
+	os.Setenv("PROMETHEUS_ADDRESS", "http://localhost:10902") // thanos-query
+
+	q := PromQLQuery{
+		Metric: "up",
+		Params: map[string]string{"container": "prometheus"}}
+
 	want := float64(1)
-	if got := Query(`up{container="prometheus"}`); got != want {
+
+	if got := Query(q.String()); got != want {
 		t.Errorf("Query() = %v, want %v", got, want)
 	}
 }
