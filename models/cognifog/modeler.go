46334f887e002f9c37a38d81a1adbc09850af6bf
Carlos Velázquez Fernández <carlos.velazquez@atos.net>
2023-11-14 18:09:01 +0100

    25 cognifog infra model first version (#29)
    * dummy commit
    
    * Implemented gRPC server
    
    * Implemented gRPC server
    
    * Fixed data collection for http server
    
    * Fixed protobuf bugs
    
    * Fixed data collection for http server
    
    * Add suggestions prior to merge
    
    * Basic functional cognifog modeler
    
    * Merge branch 'develop' into 25-cognifog-infra-model-first-version
    
    ---------
    
    Co-authored-by: Alberto Llamedo <alberto.llamedo@eviden.com>
    Co-authored-by: Alberto Llamedo Gonzalez <alberto.llamedo@atos.net>

--- models/cognifog/modeler.go
+++ models/cognifog/modeler.go
@@ -8,77 +8,103 @@ import (
 func GetInfra() *pb.InfrastructureModel {
 
 	// Get metrics from Thanos
-	//clusters := queryThanos()
-	//println(clusters)
+	infra := queryThanos()
 
 	// Convert metrics to server.proto structs
-	newClusters := ConvertMetrics( /*clusters*/ )
+	newInfra := ConvertMetrics(infra)
 
-	return newClusters
+	return newInfra
 }
 
 func queryThanos() map[string]Cluster {
 
-	// Create clusters and nodes
-	q := querier.PromQLQuery{
-		Metric: "kube_pod_info{created_by_name='prom-node-exporter'}",
-		Params: map[string]string{}}
-
 	var clusters = map[string]Cluster{}
 
-	for _, pod := range querier.Query(q.String()) {
-
-		ins := string(pod.Metric["instance"])
-		node := string(pod.Metric["node"])
-
-		// Create new cluster if needed
-		// Same instances means nodes are in the same cluster
-		// New instances are new clusters
-		if _, exists := clusters[ins]; !exists {
-			var newCluster = Cluster{
-				Name: ins,
-				Node: map[string]Node{},
-			}
-			clusters[ins] = newCluster
-		}
-
-		// Create new node
-		newNode := Node{
-			Name:          node,
-			StaticMetrics: StaticMetrics{},
-		}
-		clusters[ins].Node[node] = newNode
-	}
-
-	// Add node stats
-	q = querier.PromQLQuery{
-		Metric: "machine_cpu_cores{service='prom-kube-prometheus-kubelet'}",
+	// Create clusters and nodes
+	q := querier.PromQLQuery{
+		Metric: "kube_node_info",
 		Params: map[string]string{}}
 
 	for _, node := range querier.Query(q.String()) {
 
-		nodeName := string(node.Metric["node"])
+		cluster_id := string(node.Metric["icos_agent_cluster_id"])
+		node_name := string(node.Metric["k8s_node_name"])
 
-		for cluster := range clusters {
-			if n, exists := clusters[cluster].Node[nodeName]; exists {
-				n.StaticMetrics.CPUCores = float64(node.Value)
-				clusters[cluster].Node[nodeName] = n
+		if _, exists := clusters[cluster_id]; !exists {
+			var newCluster = Cluster{
+				Nodes: map[string]Node{},
 			}
+			clusters[cluster_id] = newCluster
 		}
 
+		// Create new node
+		newNode := Node{
+			Id:                  node_name,
+			Resources:           ComputeResources{},
+			Available_resources: ComputeResources{},
+		}
+		clusters[cluster_id].Nodes[node_name] = newNode
+	}
+
+	// Get total memory
+	q = querier.PromQLQuery{
+		Metric: "node_memory_MemTotal_bytes",
+		Params: map[string]string{}}
+
+	for _, result := range querier.Query(q.String()) {
+
+		node_name := string(result.Metric["k8s_node_name"])
+		cluster_name := string(result.Metric["icos_agent_cluster_id"])
+
+		node := clusters[cluster_name].Nodes[node_name]
+		node.Resources.MemoryInBytes = int64(result.Value)
+		clusters[cluster_name].Nodes[node_name] = node
+	}
+
+	// Get available memory
+	q = querier.PromQLQuery{
+		Metric: "node_memory_MemAvailable_bytes",
+		Params: map[string]string{}}
+
+	for _, result := range querier.Query(q.String()) {
+
+		node_name := string(result.Metric["k8s_node_name"])
+		cluster_name := string(result.Metric["icos_agent_cluster_id"])
+
+		node := clusters[cluster_name].Nodes[node_name]
+		node.Available_resources.MemoryInBytes = int64(result.Value)
+		clusters[cluster_name].Nodes[node_name] = node
 	}
 
 	return clusters
 }
 
-func ConvertMetrics( /*clusters map[string]Cluster*/ ) *pb.InfrastructureModel {
+func ConvertMetrics(clusters map[string]Cluster) *pb.InfrastructureModel {
 
-	var model = &pb.InfrastructureModel{
-		Nodes: []*pb.InfrastructureModel_Node{
-			{Id: "id1", NodeType: 1},
-			{Id: "id2", NodeType: 0},
-		},
+	// Create proto strcuture
+	var infra = &pb.InfrastructureModel{
+		Nodes: []*pb.InfrastructureModel_Node{},
 	}
 
-	return model
+	// Fill strcuture with collected data
+	for _, cluster := range clusters {
+		for _, node := range cluster.Nodes {
+			newNode := pb.InfrastructureModel_Node{
+				Id:       node.Id,
+				NodeType: pb.InfrastructureModel_Node_NodeType(node.Node_type),
+				Resources: &pb.ComputeResources{
+					MilliCPU:      node.Resources.MilliCPU,
+					MemoryInBytes: node.Resources.MemoryInBytes,
+				},
+				AvailableResources: &pb.ComputeResources{
+					MilliCPU:      node.Available_resources.MilliCPU,
+					MemoryInBytes: node.Available_resources.MemoryInBytes,
+				},
+				Labels: node.Labels,
+			}
+			infra.Nodes = append(infra.Nodes, &newNode)
+		}
+	}
+
+	return infra
 }
--- models/cognifog/models.go
+++ models/cognifog/models.go
@@ -1,101 +1,25 @@
 package models_cognifog
 
-type Controllers []*Controller
+type NodeType int
 
-type Controller struct {
-	Type                  string                `json:"type,omitempty"`
-	Name                  string                `json:"name,omitempty"`
-	Location              Location              `json:"location,omitempty"`
-	ServiceLevelAgreement ServiceLevelAgreement `json:"serviceLevelAgreement,omitempty"`
-	API                   API                   `json:"API,omitempty"`
-	Any                   any                   `json:"any,omitempty"`
-}
-
-type Agents []*Cluster
+const (
+	COMPUTE NodeType = iota
+	NETWORK
+)
 
 type Cluster struct {
-	Type                  string                `json:"type,omitempty"`
-	Name                  string                `json:"name,omitempty"`
-	Location              Location              `json:"location,omitempty"`
-	ServiceLevelAgreement ServiceLevelAgreement `json:"serviceLevelAgreement,omitempty"`
-	API                   API                   `json:"API,omitempty"`
-	Node                  map[string]Node       `json:"node,omitempty"`
-	Pod                   map[string]Pod        `json:"pod,omitempty"`
-	Any                   any                   `json:"any,omitempty"`
-}
-
-type Pod struct {
-	Status             string               `json:"status,omitempty"`
-	NumberOfContainers float64              `json:"numberOfContainers,omitempty"`
-	NumberOfApps       float64              `json:"numberOfApps,omitempty"`
-	Container          map[string]Container `json:"container,omitempty"`
-}
-
-type Container struct {
-	Name            string `json:"name,omitempty"`
-	Node            string `json:"node,omitempty"`
-	Port            []Port `json:"port,omitempty"`
-	ContainerMemory string `json:"containerMemory,omitempty"`
-	CPUUsage        string `json:"cpuUsage,omitempty"`
-	IP              string `json:"ip,omitempty"`
-}
-
-type Port struct {
-	Port string `json:"port,omitempty"`
-}
-
-type Location struct {
-	Name      string  `json:"name,omitempty"`
-	Continent string  `json:"continent,omitempty"`
-	Country   string  `json:"country,omitempty"`
-	City      string  `json:"city,omitempty"`
-	Latitude  float64 `json:"latitude,omitempty"`
-	Longitude float64 `json:"longitude,omitempty"`
+	Nodes map[string]Node `json:"nodes,omitempty"`
 }
 
 type Node struct {
-	Type           string         `json:"type,omitempty"`
-	Name           string         `json:"name,omitempty"`
-	StaticMetrics  StaticMetrics  `json:"staticMetrics,omitempty"`
-	DynamicMetrics DynamicMetrics `json:"dynamicMetrics,omitempty"`
+	Id                  string           `json:"id,omitempty"`
+	Node_type           NodeType         `json:"node_type,omitempty"`
+	Resources           ComputeResources `json:"resources,omitempty"`
+	Available_resources ComputeResources `json:"available_resources,omitempty"`
+	Labels              []string         `json:"labels,omitempty"`
 }
 
-type StaticMetrics struct { //TODO: separate in []CPU?, []GPU
-	CPUCores        float64   `json:"cpuCores,omitempty"`
-	CPUMaxFrecuency string    `json:"cpuMaxFrecuency,omitempty"`
-	GPUCores        float64   `json:"gpuCores,omitempty"`
-	GPUMaxFrecuency string    `json:"gpuMaxFrecuency,omitempty"`
-	GPURAMMemory    string    `json:"gpuRAMMemory,omitempty"`
-	RAMMemory       string    `json:"RAMMemory,omitempty"`
-	Storage         []Storage `json:"storage,omitempty"`
-}
-
-type DynamicMetrics struct { //TODO: separate in []CPU?, []GPU
-	CPUFrecuency         string         `json:"cpuFrecuency,omitempty"`
-	CPUTemperature       float64        `json:"cpuTemperature,omitempty"`
-	CPUEnergyConsumption float64        `json:"cpuEnergyConsumption,omitempty"`
-	GPUFrecuency         string         `json:"gpuFrecuency,omitempty"`
-	GPUTemperature       float64        `json:"gpuTemperature,omitempty"`
-	GPUEnergyConsumption float64        `json:"gpuEnergyConsumption,omitempty"`
-	RAMUsage             string         `json:"ramUsage,omitempty"`
-	NetworkUsage         []NetworkUsage `json:"networkUsage,omitempty"`
-}
-
-type Storage struct { //TODO: complete
-}
-
-type NetworkUsage struct { //TODO: complete
-
-}
-
-type API struct {
-	CommunicationProtocol string `json:"commProtocol,omitempty"`
-	ProtocolVersion       string `json:"protocolVersion,omitempty"`
-	DataFormat            string `json:"dataFormat,omitempty"`
-	Authentication        string `json:"authentication,omitempty"`
-	Authorization         string `json:"authorization,omitempty"`
-}
-
-type ServiceLevelAgreement struct { //TODO: complete
-	Name string `json:"name,omitempty"`
+type ComputeResources struct {
+	MilliCPU      int32 `json:"milliCPU,omitempty"`
+	MemoryInBytes int64 `json:"memoryInBytes,omitempty"`
 }
